09b241f6718b235a496974899667095f29e1ece0
dialfoar <dialfoar@gmail.com>
2014-05-25 15:25:07 +0000

    linea hola mundo

--- Inmobiliaria_fase01/src/Cartera_y_caja/Index.java
+++ Inmobiliaria_fase01/src/Cartera_y_caja/Index.java
@@ -24,6 +24,7 @@ public class Index extends javax.swing.JDialog {
         setSize(950,650);
         setLocationRelativeTo(rootPane);
         jLabel1.requestFocus();
+        System.out.print("hola mundo");
     }
 
     /**
