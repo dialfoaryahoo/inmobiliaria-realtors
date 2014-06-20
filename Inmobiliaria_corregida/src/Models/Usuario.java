59492040f61078937ed643d729ef5a3b89eac5eb
dialfoar <dialfoar@gmail.com>
2014-06-20 13:40:50 +0000

    Buscador administrar

---
+++ Inmobiliaria_corregida/src/Models/Usuario.java
@@ -0,0 +1,63 @@
+
+package Models;
+
+public class Usuario {
+   private String nombre;
+   private String usuario;
+   private String contraseña;
+   private String nivel;
+   private String ultacceso;
+   private String fecha_creacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public String getUltacceso() {
+        return ultacceso;
+    }
+
+    public String getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setUltacceso(String ultacceso) {
+        this.ultacceso = ultacceso;
+    }
+
+    public void setFecha_creacion(String fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public Usuario() {
+    }
+   
+}
